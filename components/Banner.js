1505f7f13fe2a8a366d2e168555e18091cfef7fb
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2020-08-02 21:20:12 -0300

    Update header

---
+++ components/Banner.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import styles from './Banner.module.scss'
+import classNames from 'classnames';
+
+export default function Banner() {
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const currentHour = (new Date()).getHours();
+  const isNight = (currentHour > 18 || currentHour < 6);
+  const cartoons = [
+    styles.cartoonCoffeeInTheHand,
+    styles.cartoonNormal,
+    styles.cartoonDrinkingCoffee
+  ]
+
+  useEffect(() => setRandomNumber(Math.floor(Math.random() * cartoons.length)));
+
+  return (
+    <header className={styles.header} role="banner">
+      <div className={ classNames(styles.headerBanner, { [`${ styles.headerBannerNight }`]: isNight }) }>
+        <div className={ classNames(styles.cartoon, cartoons[randomNumber]) }></div>
+        <h1 className={ styles.title }><span>Rodrigo Boniatti</span> <span>Software Engineer</span></h1>
+      </div>
+    </header>
+  )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,31 @@
 import Head from 'next/head'
 import Header from './Header'
+import Banner from './Banner'
 
 export default function Layout({ children, pageTitle, ...props }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{pageTitle}</title>
+        <meta key="charset" charset="utf-8" />
+        <meta key="description" name="description" content="Rodrigo Boniatti | Software Engineer | Ruby on Rails | Python | Go | JavaScript " />
+        <meta key="author" name="author" content="Rodrigo Boniatti" />
+        <link key="icon" rel="shortcut icon" type="type/png" href="/favicon.png" />
+        <link key="fonts" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600|Open+Sans:400,600" />
+        <title>Rodrigo Boniatti</title>
       </Head>
       <section className="layout">
         <Header />
-
+        <Banner />
         <div className="content">{children}</div>
       </section>
-      <footer>Built by me!</footer>
+
+      <footer className="footer" role="contentinfo">
+        <div className="social-media clearfix">
+          <a className="ir social-media__github" href="https://github.com/boniattirodrigo" target="_blank">Github</a>
+          <a className="ir social-media__linkedin" href="https://br.linkedin.com/in/rodrigoboniatti" target="_blank">Linkedin</a>              <a className="ir social-media__twitter" href="https://twitter.com/boniattirodrigo" target="_blank">Twitter</a>
+        </div>
+        <address><a href="mailto:boniatti.rodrigo@gmail.com">boniatti.rodrigo@gmail.com</a></address>
+      </footer>
     </>
   )
 }
